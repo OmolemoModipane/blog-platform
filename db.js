2c107e5bfea908ee0a84c3ca053fb13c88dba966
Omolemo Modipane <modipane101@gmail.com>
2024-08-11 23:00:16 +0200

    "Updated db.js file and package-lock.json with new dependencies and versions"

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql2');
 
-// Create a connection pool
+// connection pool
 const pool = mysql.createPool({
-    host: 'localhost',         // Replace with your MySQL server address
-    user: 'root',              // Replace with your MySQL username
-    password: 'Omolemo01#',    // Replace with your MySQL password
-    database: 'blog_database', // Replace with your MySQL database name
-    connectionLimit: 10        // Optional: Set the maximum number of connections in the pool
+    host: 'localhost',         
+    user: 'root',              
+    password: 'Omolemo01#',   
+    database: 'blog_database', 
+    connectionLimit: 10        // number of connections in the pool
 });
 
 // Promisify the pool query function
